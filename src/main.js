8f8c15ca6fc2623233b1cf21b297928f2099818f
vavafish <3118874165@qq.com>
2022-01-19 23:34:55 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less' // 全局样式表
+import Vant from 'vant' // 加载vant组件库
+import 'vant/lib/index.css' // 加载vant样式
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
